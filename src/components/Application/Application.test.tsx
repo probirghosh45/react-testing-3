f18e80b363a23a0db5887b577f8a90c8a27248f7
probir ghosh <probirghosh@outlook.com>
2023-03-29 18:12:42 +0600

    html element tested successfully

--- src/components/Application/Application.test.tsx
+++ src/components/Application/Application.test.tsx
@@ -1,12 +1,20 @@
-import { render , screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Application } from "./Application";
 
-describe("Application Info", () => {
+describe("Application", () => {
   test("render correctly", () => {
     render(<Application />);
-    const nameElement = screen.getByRole('textbox')
+    const nameElement = screen.getByRole("textbox"); // getByRole ="textbox" for input element
     expect(nameElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox"); // getByRole ="combobox" for dropdown
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const termsElement = screen.getByRole("checkbox"); // getByRole ="checkbox" for checkbox
+    expect(termsElement).toBeInTheDocument();
+
+    const submitElement = screen.getByRole("button"); // getByRole ="button" for submit button
+    expect(submitElement).toBeInTheDocument();
+
   });
 });
-
-
